31fa1dacf0d2c93854edcbeaf8edbee56d23aaf6
vuusale <vuusale@gmail.com>
2021-10-21 16:13:35 +0400

    Feature/lab5 fixe

--- src/test/java/com/example/springproj1/SimpleIntegrationTest.java
+++ src/test/java/com/example/springproj1/SimpleIntegrationTest.java
@@ -7,10 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.*;
@@ -20,56 +17,26 @@ import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SimpleIntegrationTest {
-
-    Set<String> parameters1 = new HashSet<>(Arrays.asList("id", "createdAt", "name"));
-    Set<String> parameters2 = new HashSet<>(Arrays.asList("id", "repoId", "createdAt", "name", "updatedAt"));
-    Set<String> parameters3 = new HashSet<>(Arrays.asList("id", "branchId", "createdAt", "message", "entry", "sha"));
-    Map<String, Set<String>> urls  = new HashMap<String, Set<String>>() {{
-        put("/url1", parameters1);
-        put("/url2", parameters2);
-        put("/url3", parameters3);
-    }};
-
-    @LocalServerPort
-    private int port;
+    
+    String[] urls = {
+            "https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos",
+            "https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos/1/branches",
+            "https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos/1/branches/1/commits"
+    };
 
     TestRestTemplate restTemplate = new TestRestTemplate();
 
     HttpHeaders headers = new HttpHeaders();
 
     @Test
-    public void testUrls() throws Exception {
-        for (Map.Entry<String, Set<String>> entry : urls.entrySet()) {
-            String url = entry.getKey();
-            Set<String> parameters = entry.getValue();
-
+    public void testUrls() {
+        for (String url : urls) {
             HttpEntity<String> entity = new HttpEntity<>(null, headers);
 
             ResponseEntity<String> response = restTemplate.exchange(
-                    createURLWithPort(url), HttpMethod.GET, entity, String.class);
+                    url, HttpMethod.GET, entity, String.class);
 
-            String actual = response.getBody();
-
-            assertTrue(testValid(parameters, actual));
+            assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
         }
-    }
-
-    private String createURLWithPort(String uri) {
-        return "http://localhost:" + port + uri;
-    }
-
-    public boolean testValid(Set<String> parameters, String response) {
-        JSONArray objects = new JSONArray(response);
-        for (Object object : objects) {
-            JSONObject obj = (JSONObject) object;
-            Iterator<String> keys = obj.keys();
-            while(keys.hasNext()) {
-                String property = keys.next();
-                if (!parameters.contains(property)) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
