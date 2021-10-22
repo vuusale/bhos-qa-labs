535d5da7bb8ef33372bde09b1b5e14c4c5b2d375
vuusale <vuusale@gmail.com>
2021-10-22 11:13:02 +0400

    SSL pinning

--- src/test/java/com/example/springproj2/SimpleIntegrationTest.java
+++ src/test/java/com/example/springproj2/SimpleIntegrationTest.java
@@ -7,13 +7,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import java.io.*;
+import java.net.URL;
+import java.security.cert.*;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,16 +26,46 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SimpleIntegrationTest {
 
+    public String url = "https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos";
+
     TestRestTemplate restTemplate = new TestRestTemplate();
 
     HttpHeaders headers = new HttpHeaders();
+
+    @Test
+    public void testSSLPinning() throws IOException {
+        String encodedCert = "";
+        String everything;
+        try {
+            URL https_url = new URL(url);
+            HttpsURLConnection con = (HttpsURLConnection) https_url.openConnection();
+            con.connect();
+            Certificate[] certs = con.getServerCertificates();
+            Certificate cert = certs[0];
+            encodedCert = Base64.getEncoder().encodeToString(cert.getEncoded());
+        } catch (SSLPeerUnverifiedException | CertificateEncodingException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader("cert.cer"))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            everything = sb.toString();
+        }
+        assertEquals(everything.strip(), encodedCert.strip());
+    }
 
     @Test
     public void testUrl() throws JSONException {
         HttpEntity<String> entity = new HttpEntity<>(null, headers);
 
         ResponseEntity<String> response = restTemplate.exchange(
-                "https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos", HttpMethod.GET, entity, String.class);
+                url, HttpMethod.GET, entity, String.class);
 
         String actual = response.getBody();
         assertTrue(testJson(actual));
