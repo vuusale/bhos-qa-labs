2b7b6a6baf322a3d9f03200a2aa3d70acd2ef812
vuusale <vuusale@gmail.com>
2021-10-22 18:57:46 +0400

    Feature/lab7

--- src/main/java/com/example/springproj3/FirebaseInitialize.java
+++ src/main/java/com/example/springproj3/FirebaseInitialize.java
@@ -3,8 +3,6 @@ package com.example.springproj3;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.gson.JsonObject;
-import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -18,15 +16,15 @@ public class FirebaseInitialize {
     @PostConstruct
     public void initialize() {
         try {
-            FileWriter myWriter = new FileWriter("creds.json");
-            myWriter.write(System.getenv("FIREBASE_CREDENTIALS"));
-            myWriter.close();
+//            FileWriter myWriter = new FileWriter("creds.json");
+//            myWriter.write(System.getenv("FIREBASE_CREDENTIALS"));
+//            myWriter.close();
+//            FileInputStream serviceAccount = new FileInputStream("creds.json");
 //            FileInputStream serviceAccount = new FileInputStream("bhos-qa-labs-firebase-adminsdk-ebidy-71fbaceed6.json");
-            FileInputStream serviceAccount = new FileInputStream("creds.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(System.getenv("FIREBASE_CREDENTIALS").getBytes())))
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(System.getenv("FIREBASE_CREDENTIALS").getBytes())))
                     .setDatabaseUrl("https://bhos-qa-labs.firebaseio.com")
                     .build();
 
