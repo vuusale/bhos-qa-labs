e8e6c42bc4821ea77dc464d2ecd010c703d5c3d3
vuusale <vuusale@gmail.com>
2021-10-22 18:43:24 +0400

    Feature/lab7

--- src/main/java/com/example/springproj3/FirebaseInitialize.java
+++ src/main/java/com/example/springproj3/FirebaseInitialize.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 
 @Service
 public class FirebaseInitialize {
@@ -17,23 +18,15 @@ public class FirebaseInitialize {
     @PostConstruct
     public void initialize() {
         try {
+            FileWriter myWriter = new FileWriter("creds.json");
+            myWriter.write(System.getenv("FIREBASE_CREDENTIALS"));
+            myWriter.close();
 //            FileInputStream serviceAccount = new FileInputStream("bhos-qa-labs-firebase-adminsdk-ebidy-71fbaceed6.json");
-
-            JSONObject creds = new JSONObject();
-            creds.put("type", System.getenv("FIREBASE_TYPE"));
-            creds.put("project_id", System.getenv("FIREBASE_PROJECT_ID"));
-            creds.put("private_key_id", System.getenv("FIREBASE_KEY_ID"));
-            creds.put("private_key", System.getenv("FIREBASE_PRIVATE_KEY"));
-            creds.put("client_email", System.getenv("FIREBASE_CLIENT_EMAIL"));
-            creds.put("client_id", System.getenv("FIREBASE_CLIENT_ID"));
-            creds.put("auth_uri", System.getenv("FIREBASE_AUTH_URI"));
-            creds.put("token_uri", System.getenv("FIREBASE_TOKEN_URI"));
-            creds.put("auth_provider_x509_cert_url", System.getenv("FIREBASE_CERT_URL"));
-            creds.put("client_x509_cert_url", System.getenv("FIREBASE_CLIENT_CERT_URL"));
+            FileInputStream serviceAccount = new FileInputStream("creds.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
-//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(creds.toString().getBytes())))
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(System.getenv("FIREBASE_CREDENTIALS").getBytes())))
                     .setDatabaseUrl("https://bhos-qa-labs.firebaseio.com")
                     .build();
 
