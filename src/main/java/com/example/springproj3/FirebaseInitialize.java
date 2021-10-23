0762ba8c08b426487fe62ecde5092b4e0c870c86
vuusale <vuusale@gmail.com>
2021-10-23 09:39:36 +0400

    Feature/lab7

--- src/main/java/com/example/springproj3/FirebaseInitialize.java
+++ src/main/java/com/example/springproj3/FirebaseInitialize.java
@@ -16,15 +16,15 @@ public class FirebaseInitialize {
     @PostConstruct
     public void initialize() {
         try {
-            FileWriter myWriter = new FileWriter("creds.json");
-            myWriter.write(System.getenv("FIREBASE_CREDENTIALS"));
-            myWriter.close();
-            FileInputStream serviceAccount = new FileInputStream("creds.json");
+//            FileWriter myWriter = new FileWriter("creds.json");
+//            myWriter.write(System.getenv("FIREBASE_CREDENTIALS"));
+//            myWriter.close();
+//            FileInputStream serviceAccount = new FileInputStream("creds.json");
 //            FileInputStream serviceAccount = new FileInputStream("bhos-qa-labs-firebase-adminsdk-ebidy-71fbaceed6.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(System.getenv("FIREBASE_CREDENTIALS").getBytes())))
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(System.getenv("FIREBASE_CREDENTIALS").getBytes())))
                     .setDatabaseUrl("https://bhos-qa-labs.firebaseio.com")
                     .build();
 
